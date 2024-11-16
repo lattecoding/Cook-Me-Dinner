c09beb28d44bda255fc838374a905f936b190770
lattepunk-coding <174910592+lattepunk-coding@users.noreply.github.com>
2024-11-16 15:50:39 -0500

    added myself as a seeded user

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -1,9 +1,13 @@
-import { User } from '../models/user.js';
+import { User } from "../models/user.js";
 
 export const seedUsers = async () => {
-  await User.bulkCreate([
-    { username: 'jessica', password: 'password' },
-    { username: 'netra', password: 'password' },
-    { username: 'talon', password: 'password' },
-  ], { individualHooks: true });
+  await User.bulkCreate(
+    [
+      { username: "jessica", password: "password" },
+      { username: "netra", password: "password" },
+      { username: "talon", password: "password" },
+      { username: "luis", password: "password" },
+    ],
+    { individualHooks: true },
+  );
 };
