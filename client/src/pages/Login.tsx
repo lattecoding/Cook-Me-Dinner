fea1d97e1b5f5275a759003130290bfa4c271cc8
naiknetra <naiknetra5@gmail.com>
2024-11-20 11:11:17 -0500

    added image to login

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -34,7 +34,11 @@ const Login = () => {
   };
 
   return (
+    <main className="ktool">
+      
     <div className="container">
+      
+          
       <form className="form" onSubmit={handleSubmit}>
         <h1>Login</h1>
         <label>Username</label>
@@ -53,7 +57,10 @@ const Login = () => {
         />
         <button type="submit">Submit Form</button>
       </form>
+      
+      
     </div>
+    </main>
   );
 };
 
