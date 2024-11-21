f1b4e03dab2a5294d499bf7b8fe1528c2df04925
toasterbrain28 <talonbarkie28@gmail.com>
2024-11-20 19:10:12 -0700

    removed extra button

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary w-100">
       <div className="container-lg">
-        <a className="navbar-brand" href="#">Cookify</a>
+        <a className="navbar-brand" href="#"> Cookify</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -47,9 +47,7 @@ const Navbar = () => {
       <span className="me-3">
         Hey, {auth.getProfile()?.username || "User"}!
       </span>
-      <button className="btn btn-outline-success me-2" type="submit">
-        Spotify
-      </button>
+      
       <button
         className="btn btn-outline-secondary"
         type="button"
